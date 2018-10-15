791bf467ea6ab40c8af48b2441e2e81519b8defa
marcelomenezes <marcelohfms@gmail.com>
2018-10-14 21:44:25 -0300

    Initial commit

---
+++ app/src/main/java/xie/araca/logopeia/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package xie.araca.logopeia.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xie.araca.logopeia.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
