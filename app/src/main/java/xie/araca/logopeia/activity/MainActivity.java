c4019065852b7e0821d3916ec4b7db557c512e39
marcelomenezes <marcelohfms@gmail.com>
2018-11-13 20:41:38 -0300

    Adicionada flow do projeto

--- app/src/main/java/xie/araca/logopeia/activity/MainActivity.java
+++ app/src/main/java/xie/araca/logopeia/activity/MainActivity.java
@@ -20,9 +20,10 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ToolbarUnderActivity implements View.OnClickListener{
 
     private DatabaseReference referenceFirebase;
+    private ToolbarUnderActivity toolbarUnderActivity = new ToolbarUnderActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,50 +31,29 @@ public class MainActivity extends AppCompatActivity {
         ActivityEventBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         referenceFirebase = ConfigFirebase.getFirebase();
         referenceFirebase.child("kawhiL").setValue(1000);
-        startToolbarUnder();
 
     }
 
-    public void startToolbarUnder(){
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.toolbarMain.btHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
-        ActivityEventBinding bindingEvent = DataBindingUtil.setContentView(this, R.layout.activity_event);
-        bindingEvent.toolbarEvent.btEvents.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), EventActivity.class);
-                startActivity(intent);
-            }
-        });
-        ActivityFavoriteBinding bindingFavorite = DataBindingUtil.setContentView(this, R.layout.activity_favorite);
-        bindingFavorite.toolbarFavorite.btFavorite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), FavoriteActivity.class);
-                startActivity(intent);
-            }
-        });
-        ActivityExploreBinding bindingExplore = DataBindingUtil.setContentView(this, R.layout.activity_explore);
-        bindingExplore.toolbarExplore.btExplore.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ExploreActivity.class);
-                startActivity(intent);
-            }
-        });
-        ActivityArtistBinding bindingArtists = DataBindingUtil.setContentView(this, R.layout.activity_artist);
-        bindingArtists.toolbarArtist.btArtists.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ArtistActivity.class);
-                startActivity(intent);
-            }
-        });
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+
+            case R.id.btHome:
+                btHome();
+            case R.id.btArtists:
+                btArtists();
+            case R.id.btEvents:
+                btEvents();
+            case R.id.btExplore:
+                btExplore();
+            case R.id.btFavorite:
+                btFavorite();
+            default:
+                break;
+        }
+
     }
+
 }
